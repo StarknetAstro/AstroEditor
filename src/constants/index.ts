997b6f2fe552c8635bf90ebe1e620671639d16db
YanYuan <yan950414@gmail.com>
2023-11-13 12:15:04 +0800

    chore: clean code

--- src/constants/index.ts
+++
@@ -1,7 +0,0 @@
-
-
-export const WS_ALCHEMY_URL = 'wss://eth-mainnet.g.alchemy.com/v2/';
-export const ENVS = {
-    VITE_WALLET_CONNECT_ID:import.meta.env.VITE_WALLET_CONNECT_ID,
-    VITE_ALCHEMY_ID: import.meta.env.VITE_ALCHEMY_ID,
-}
--- src/services/swap.ts
+++
@@ -1,6 +0,0 @@
-// import request from "@/utils/request.ts";
-
-
-export const getTokenList = () => {
-    return fetch('https://tokens.coingecko.com/uniswap/all.json').then(res => res.json());
-}
