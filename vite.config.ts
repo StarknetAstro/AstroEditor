7b7777f94639c1628b5293168be87b17df933793
cryptonerdcn <cryptonerdcn@gmail.com>
2023-11-28 17:58:48 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './docs'
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
